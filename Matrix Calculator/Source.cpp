ee947d876b1d5d013796c3eee2a8d274b826fa66
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2020-06-29 20:22:57 +0500

    Get order of matix A and B

--- Matrix Calculator/Source.cpp
+++ Matrix Calculator/Source.cpp
@@ -1,9 +1,40 @@
 #include<iostream>
 #include<conio.h>
+#include<stdlib.h>
 
 using namespace std;
 
+struct Matrix
+{
+	int rows_matrix, cols_matrix;
+};
+
 void main() {
-	cout << "This is a matrix calculator";
+
+	Matrix matrix_A;
+	cout << "Matrix A:"<<endl;
+	cout << "Rows = ";
+	cin >> matrix_A.rows_matrix;
+	cout << "Columns = ";
+	cin >> matrix_A.cols_matrix;
+
+	cout << endl;
+
+	Matrix matrix_B;
+	cout << "Matrix B:" << endl;
+	cout << "Rows = ";
+	cin >> matrix_B.rows_matrix;
+	cout << "Columns = ";
+	cin >> matrix_B.cols_matrix;
+
+	cout << endl;
+
 	_getch();
+	system("CLS");
+
+	cout << "Order of matrix A = " << matrix_A.rows_matrix << " * " << matrix_A.cols_matrix;
+	cout << endl;
+	cout << "Order of matrix B = " << matrix_B.rows_matrix << " * " << matrix_B.cols_matrix;
+
+
 }
