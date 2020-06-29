7137abe9f35c959812bfe68b58a9747f4b6cba35
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2020-06-29 21:16:17 +0500

    Project saved Day: 1

--- Matrix Calculator/Source.cpp
+++ Matrix Calculator/Source.cpp
@@ -7,11 +7,59 @@ using namespace std;
 struct Matrix
 {
 	int rows_matrix, cols_matrix;
+	char matrix_symbol;
 };
+
+void display_order(int r, int c,char s) {
+
+	cout << endl;
+	
+	cout << "Order of matrix "<<s<<" = " << r << " * " << c;
+
+
+}
+
+char operation() {
+
+	char operation_symobol;
+
+	cout << "Select an operation to continue:" << endl;
+	cout << "+ = SUM"<<endl;
+	cout << "- = SUBTRACTION" << endl;
+	cout << "* = PRODUCT" << endl;
+	cout << endl;
+	cout << "Operation = ";
+
+	cin >> operation_symobol;
+
+	if (operation_symobol == '+' || operation_symobol == '*' || operation_symobol == '-') {
+		return operation_symobol;
+	}
+	else {
+		return '\0';
+	}
+
+
+	
+
+}
+
+void press_to_continue() {
+	cout << endl;
+	cout << "Press any key to continue...";
+	_getch();
+}
+
+void cls() {
+	system("CLS");
+}
 
 void main() {
 
+	char operation_symbol_main='\0';
+
 	Matrix matrix_A;
+	matrix_A.matrix_symbol = 'A';
 	cout << "Matrix A:"<<endl;
 	cout << "Rows = ";
 	cin >> matrix_A.rows_matrix;
@@ -21,6 +69,7 @@ void main() {
 	cout << endl;
 
 	Matrix matrix_B;
+	matrix_B.matrix_symbol = 'B';
 	cout << "Matrix B:" << endl;
 	cout << "Rows = ";
 	cin >> matrix_B.rows_matrix;
@@ -28,13 +77,60 @@ void main() {
 	cin >> matrix_B.cols_matrix;
 
 	cout << endl;
+	
+	
+	press_to_continue();
+	cls();
+	
+	do {
+		display_order(matrix_A.rows_matrix, matrix_A.cols_matrix, matrix_A.matrix_symbol);
+		display_order(matrix_B.rows_matrix, matrix_B.cols_matrix, matrix_B.matrix_symbol);
 
-	_getch();
-	system("CLS");
 
-	cout << "Order of matrix A = " << matrix_A.rows_matrix << " * " << matrix_A.cols_matrix;
-	cout << endl;
-	cout << "Order of matrix B = " << matrix_B.rows_matrix << " * " << matrix_B.cols_matrix;
+		cout << endl;
+		cout << endl;
 
+		operation_symbol_main = operation();
+
+		
+
+		if (operation_symbol_main == '\0') {
+			cout  << endl << "ERROR: ***INVALID SELECTION***"<<endl;
+		}
+		
+		cout << "You Selected:";
+		switch (operation_symbol_main)
+		{
+		case '+':
+		{
+			 cout<<" Sum of MATRICES " ;
+			break;
+		}
+		case '-':
+		{
+			cout << " Difference of MATRICES ";
+			break;
+		}
+		case '*':
+		{
+			cout << " Product of MATRICES ";
+			break;
+		}
+		default:
+		{
+			cout << " INVALID OPERATOR ";
+			break;
+		}
+			
+		}
+
+		cout << endl;
+		press_to_continue();
+
+		if (operation_symbol_main == '\0') {
+			cls();
+		}
+	} while (operation_symbol_main == '\0');
 
 }
+
